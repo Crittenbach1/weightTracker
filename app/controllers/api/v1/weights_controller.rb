2552547cc1130aaae16f28a473e8f06e38f1dbd0
Cynthia Rittenbach <crittenbach@gmail.com>
2018-12-23 01:02:57 -0500

    eliminated destroy and update controllers

--- app/controllers/api/v1/weights_controller.rb
+++ app/controllers/api/v1/weights_controller.rb
@@ -7,16 +7,6 @@ class Api::V1::WeightsController < Api::V1::BaseController
     respond_with :api, :v1, Weight.create(weight_params)
   end
 
-  def destroy
-    respond_with Weight.destroy(params[:id])
-  end
-
-  def update
-    weight = Weight.find(params["id"])
-    weight.update_attributes(weight_params)
-    respond_with weight, json: weight
-  end
-
   private
 
   def weight_params
