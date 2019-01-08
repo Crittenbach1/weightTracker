18ab2c4c1cc22ed8c19ae11f73ccd0f8632b5ca5
Cynthia Rittenbach <crittenbach@gmail.com>
2019-01-08 00:12:33 -0500

    persons pounds must be present

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,7 @@ class Person < ActiveRecord::Base
    has_many :weights
    accepts_nested_attributes_for :weights
    validates :name, :uniqueness => true
+   validates :name, :presence => true
+   validates :pounds, :presence => true
+
 end
