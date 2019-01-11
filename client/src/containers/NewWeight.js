582cf93bd890677235e37d509b001a6b73669cd4
Cynthia Rittenbach <crittenbach@gmail.com>
2019-01-11 01:07:06 -0500

    errorCheck function uses a switchcase to check for errors being passed into chartData and addWeight

--- client/src/containers/NewWeight.js
+++ client/src/containers/NewWeight.js
@@ -27,7 +27,7 @@ class NewPerson extends Component {
     this.addPerson = this.addPerson.bind(this);
     this.handleDateChange = this.handleDateChange.bind(this);
     this.handleAddWeight = this.handleAddWeight.bind(this);
-
+    this.errorCheck = this.errorCheck.bind(this);
   }
 
 
@@ -35,6 +35,23 @@ class NewPerson extends Component {
     this.setState({
       startDate: input
     });
+  }
+
+  handleAddWeight = () => {
+    debugger
+    let weights = this.state.weights;
+    let bothDates = this.getDateData(this.state.startDate)
+    if (this.errorCheck('ADD_WEIGHT', bothDates) == false) {
+      debugger
+      weights.push({ id: this.state.weights.length + 1,
+                     currentDate: bothDates[1],
+                     pounds: '',
+                     ms: bothDates[0]
+                   })
+      this.setState({ weights: weights,
+                      error: false
+                    })
+    }
   }
 
   getDateData = (selectedDate) => {
@@ -60,32 +77,7 @@ class NewPerson extends Component {
     }
 
     let currentDate = formatDate(date);
-
     return [ms, currentDate];
-
-  }
-
-  handleAddWeight = () => {
-    let weights = this.state.weights;
-    let bothDates = this.getDateData(this.state.startDate)
-
-    debugger
-    let taken = this.state.weights.filter(function(w){ return w.currentDate === bothDates[1] });
-    let peopleEmpty = this.state.people[0];
-    if (taken.length != 0) {
-      this.setState({ error: "selected date is taken"})
-    } else if (peopleEmpty == null) {
-      this.setState({ error: "must create a person first to add weights"})
-    } else {
-      weights.push({ id: this.state.weights.length + 1,
-                     currentDate: bothDates[1],
-                     pounds: '',
-                     ms: bothDates[0]
-                   })
-      this.setState({ weights: weights,
-                      error: false
-                    })
-    }
   }
 
   handleRemoveWeight = () => {
@@ -105,17 +97,37 @@ class NewPerson extends Component {
     this.setState({ people: people });
   }
 
+  errorCheck = (type, data) => {
+    switch (type) {
+      case 'CHART_PEOPLE':
+      debugger
+          let poundsError = data.weights.filter(function(w){ return w.pounds == "" })[0] != null;
+          let weightsError = data.weights[0] == null;
+          let nameError = data.name == "";
+          if (poundsError || nameError || weightsError) {
+            this.setState({ saveData: false,
+                            chartData: [],
+                            error: "name or weights must not be empty"
+                          });
+            return true;
+          } else { return false };
+      case 'ADD_WEIGHT':
+       debugger
+          let taken = this.state.weights.filter(function(w){ return w.currentDate === data[1] });
+          let peopleEmpty = this.state.people[0];
+          if (taken.length != 0) {
+            this.setState({ error: "selected date is taken"})
+          } else if (peopleEmpty == null) {
+            this.setState({ error: "must create a person first to add weights"})
+          } else { return false };
+      default:
+        return true;
+      }
+   }
+
   chartPeople = (data) => {
     debugger
-        let poundsError = data.weights.filter(function(w){ return w.pounds == "" })[0] != null;
-        let weightsError = data.weights[0] == null;
-        let nameError = data.name == "";
-        if (poundsError || nameError || weightsError) {
-          this.setState({ saveData: false,
-                          chartData: [],
-                          error: "name or weights must not be empty"
-                        });
-        } else {
+        if (this.errorCheck("CHART_PEOPLE", data) == false) {
             let chartData = this.state.chartData;
             let dataPoints = [];
             for(let i=0; data.weights.length > i; i++) {
@@ -162,8 +174,6 @@ class NewPerson extends Component {
           <Button id="button" variant="contained" color="primary" onClick={this.addPerson}>Add Person</Button>
           <Button id="button" variant="contained" color="primary" onClick={this.removePerson}>Remove Person</Button>
           <Button id="button" variant="contained" color="primary" onClick={this.saveData} >Chart Data</Button>
-
-
           <Button id="button" variant="contained" color="primary" onClick={this.handleAddWeight}>Add Weight</Button>
           <Button id="button" variant="contained" color="primary" onClick={this.handleRemoveWeight}>Remove Weight</Button>
 
